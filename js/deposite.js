3f590eb58dc2ab4554053e9c8651b207ddc054b4
Rihanul Islam <rihanthedev@gmail.com>
2023-02-11 23:20:29 +0600

    Deposite js

--- js/deposite.js
+++ js/deposite.js
@@ -2,14 +2,25 @@
 document.getElementById('addDeposite').addEventListener('click', function(){
     // step-2: get the deposite Amount form input Filed
     const depositeFiled = document.getElementById('deposite-filed');
-    const depositeAmount = depositeFiled.value;
-
+    const NewDepositeAmountstring = depositeFiled.value;
+    const NewDepositeAmount = parseFloat(NewDepositeAmountstring)
     // step-3: get The deposite total Amount current
     // for non input (element other than input textarea use to the text innertext)
     const depositeTotalElement = document.getElementById('desposite-Total');
-    depositeTotalElement.innerText = depositeAmount;
-    
+    const previousDepositeTotalString = depositeTotalElement.innerText;
+    const previousDepositeTotal = parseFloat(previousDepositeTotalString);
 
+    const currentDepositeTotal = previousDepositeTotal + NewDepositeAmount;
+    depositeTotalElement.innerText = currentDepositeTotal;
 
-    
+    // step-5: get balance current deposite
+    const totalBalanceElement = document.getElementById('total-Balance');
+    const previousTotalBalanceString = totalBalanceElement.innerText;
+    const previousTotal = parseFloat(previousTotalBalanceString);
+    // step-6 calculete this 
+     const currentBalanceTotal = previousTotal+previousDepositeTotal;
+     totalBalanceElement.innerText = currentBalanceTotal;
+    // dwwtp:7 clear value
+    depositeFiled.value = ''
+  
 })
